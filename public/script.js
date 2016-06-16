d6a6439fb4b71dcd03b7be0aef3cbc69f55b4a6e
Sacha David <Sach1810@Sachas-MacBook-Pro.local>
2016-06-16 22:53:45 +0300

    tweaks added

--- public/script.js
+++ public/script.js
@@ -2,13 +2,15 @@ var socket = io.connect();
 var list = [];
 socket.on('new', function(obj) {
     if (obj.tracker[obj.word] == 1) {
-        $(".list").append('<div id="' + obj.word + '" class="all" '+ obj.tracker[obj.word] +'">' + obj.word + "  " + '</div>');
+        $(".list").append('<div id="' + obj.word + '" class="all" ' + obj.tracker[obj.word] + '">' + obj.word + "  " + '</div>').find('div.cloud1').fadeIn('slow');
         console.log($('#' + obj.word).data().id);
     }
 
 });
-
 socket.on('update', function(obj) {
-    $('#' + obj.word).css('font-size', obj.tracker[obj.word] * 10 + "px");
-    $('#' + obj.word).css('width', obj.tracker[obj.word] * 7 + "vw");
+    $('#' + obj.word).addClass('show');
+    $('#' + obj.word).css('font-size', obj.tracker[obj.word] * 50 + "%");
+    $('#' + obj.word).css('width', obj.tracker[obj.word] * 15 + "vw");
+    var all = Object.keys(obj.tracker);
+    $('.all').addClass('show');
 });
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(express.static('node_modules'));
 app.use('/', express.static(__dirname + '/public'));
 
 var tracker = {};
+var count;
 io.on('connection', function(socket) {
 
     console.log("a user connected");

c38f86667410c2b82e16424c318798f0b32eefd8
Sacha David <Sach1810@Sachas-MacBook-Pro.local>
2016-06-16 22:11:37 +0300

    more styling added div width made more dynamic

--- public/script.js
+++ public/script.js
@@ -2,13 +2,13 @@ var socket = io.connect();
 var list = [];
 socket.on('new', function(obj) {
     if (obj.tracker[obj.word] == 1) {
-        $(".list").append('<div id="' + obj.word + '" class="all">' + obj.word + "  " + '</div>');
+        $(".list").append('<div id="' + obj.word + '" class="all" '+ obj.tracker[obj.word] +'">' + obj.word + "  " + '</div>');
+        console.log($('#' + obj.word).data().id);
     }
 
 });
 
 socket.on('update', function(obj) {
-        obj.tracker[obj.word] 
-
     $('#' + obj.word).css('font-size', obj.tracker[obj.word] * 10 + "px");
+    $('#' + obj.word).css('width', obj.tracker[obj.word] * 7 + "vw");
 });
